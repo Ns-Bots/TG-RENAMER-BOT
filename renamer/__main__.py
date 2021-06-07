b83f35d94a69a1a85b660c728aa0adefdbc6e9f9
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-07 20:07:03 +0530

    Update __main__.py

--- renamer/__main__.py
+++ renamer/__main__.py
@@ -27,7 +27,7 @@ def main():
     Renamer.start()
     me = Renamer.get_me()
 
-    startup_msg = f"Successfully deployed your Renamer in @{me.username}\n"
+    startup_msg = f"Successfully deployed your Renamer at @{me.username}\n"
     startup_msg += f"Pyrogram Version: V{__version__}\n"
     startup_msg += f"Python Version: V{platform.python_version()}\n\n"
     startup_msg += "Thanks for deploying our bot. Please give a start to my repo and join @Ns_bot_updates."
