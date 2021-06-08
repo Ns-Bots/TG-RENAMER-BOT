598c3bdd72b598a0761822bdbd921a950c4fff5e
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-08 07:51:22 +0530

    Fixed typo

--- renamer/plugins/commands.py
+++ renamer/plugins/commands.py
@@ -40,7 +40,7 @@ async def help(c, m, cb=False):
 @RenamerNs.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(c, m, cb=False):
     owner = await c.get_users(Config.OWNER_ID)
-    owner_username = owner.username else 'Ns_bot_updates'
+    owner_username = owner.username if owner.username else 'Ns_bot_updates'
     button = [[
         InlineKeyboardButton(f'{MAN_TEACHER_LIGHT_SKIN_TONE} My Owner', url=f'https://t.me/{owner_username}'),
         InlineKeyboardButton(f'{ROBOT} About', callback_data='about')
