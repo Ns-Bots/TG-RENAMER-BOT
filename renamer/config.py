3d6768ae671c950b1f0e7896db63175584a30c40
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-08 07:18:25 +0530

    Update config.py

--- renamer/config.py
+++ renamer/config.py
@@ -7,7 +7,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     OWNER_ID =  int(os.environ.get("OWNER_ID", ""))
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")] if os.environ.get("AUTH_USERS", "") else []
+    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")] #if os.environ.get("AUTH_USERS", "") else []
     AUTH_USERS = AUTH_USERS.append(OWNER_ID)
     print(AUTH_USERS)
     BANNED_USERS = [int(i) for i in os.environ.get("BANNED_USERS", "").split(" ")] if os.environ.get("BANNED_USERS", "") else None
