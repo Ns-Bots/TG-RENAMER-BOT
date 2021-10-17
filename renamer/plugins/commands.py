0aba29c4471335f38792cd886cd0be6420fec443
Vampire421 <91833744+Vampire421@users.noreply.github.com>
2021-10-17 13:22:46 +0530

    Update commands.py

--- renamer/plugins/commands.py
+++ renamer/plugins/commands.py
@@ -40,9 +40,9 @@ async def help(c, m, cb=False):
 @RenamerNs.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(c, m, cb=False):
     owner = await c.get_users(Config.OWNER_ID)
-    owner_username = owner.username if owner.username else 'Ns_bot_updates'
+    owner_username = owner.username if owner.username else 'HF_Projects'
     button = [[
-        InlineKeyboardButton(f'{MAN_TEACHER_LIGHT_SKIN_TONE} My Owner', url=f'https://t.me/{owner_username}'),
+        InlineKeyboardButton(f'{MAN_TEACHER_LIGHT_SKIN_TONE} My Owner', url=f'https://t.me/saminsumesh'),
         InlineKeyboardButton(f'{ROBOT} About', callback_data='about')
         ],[
         InlineKeyboardButton(f'{INFORMATION} Help', callback_data="help"),
