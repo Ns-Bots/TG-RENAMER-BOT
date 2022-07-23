9a11888bf9abba025ac7540e34604f3d174daaf0
sbdipun9090 <100336442+sbdipun9090@users.noreply.github.com>
2022-07-23 12:25:34 +0530

    Update text.py

--- renamer/tools/text.py
+++ renamer/tools/text.py
@@ -22,9 +22,9 @@ class TEXT:
 
 ** Developer:** {bot_owner}
 
-** Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+** Channel:** [CineHub4U](https://t.me/CineHub4U)
 
-** Group:** [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+** Group:** [CineHub4u-ChitChat](https://t.me/CineHub4U_Group)
 
 ** Source Code:** [Press Me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)
 """
@@ -49,14 +49,4 @@ I am a telegram renamer with permanent thumbnail.
 """
 
 
-    DONATE_USER = """**__Thanks for showing interest in donation.__**
  
-Donate us to keep our services continously alive
-You can send any amount 
-of 20rs, 30rs, 50rs, 70rs, 100rs, 200rs
- 
-__--Payment Methods:--__
- 
-GooglePay / Paytm / PhonPay / paypal / Net Banking
- 
-**For Donate:** message me @MaxxRiderz"""
